1c6b413d7e69fe5b75345627b24f499e51c8ded5
Macaron Chef <macaronswap@gmail.com>
2021-03-13 15:26:19 +0300

    whitelist check page completed

--- whitelist.js
+++ whitelist.js
@@ -1,176 +1,31 @@
-let LOADING = false
-let IS_ACTIVE = false
-let TOTAL_FOR_SALE = 1000000  //Total token count for sale, using for progress
+let _addressList = [
+  '0x4430b3230294D12c6AB2aAC5C2cd68E80B16b581',
+  '0x90F729F31e20C58b93A7092f1e10bdAE9CC4d82B',
+  '0x107830BC52E2FC449336F8Fd039939c841d4131C'
+]
 
 $(async () => {
-  setLoading(false)
+  $('#loader').hide()
+  $('#content').show()
+  $('#input-address').keyup(checkWhitelistStatus)
 })
 
-async function load() {
-  await loadReadContract()
-  // await loadAccount()
-  // await loadContract()
-  await render()
-  setInterval(setRemainingTokens,10000);
-}
-
-async function loadReadContract() {
-  ABI = await $.getJSON('CrowdSale.json')
-
-  READ_CONTRACT = new ethers.Contract(CONTRACT_ADDRESS, ABI, READ_WEB3_PROVIDER)
-}
-
-async function loadWriteContract() {
-  WRITE_CONTRACT = new ethers.Contract(
-    CONTRACT_ADDRESS,
-    ABI,
-    WRITE_WEB3_PROVIDER.getSigner()
-  )
-}
-
-function showWallets() {
-  $('#connect-wallets').removeClass('hidden')
-}
-
-function closeWallets() {
-  $('#connect-wallets').addClass('hidden')
-}
-
-async function connectWallet() {
-  showWallets()
-}
-
-async function connectMetamask() {
-  if (!window.ethereum)
-    return sl('error', 'Please install the Metamask extension')
-  await window.ethereum.enable()
-  closeWallets()
-  await loadAccount(window.ethereum)
-}
-
-async function connectWalletConnect() {
-  const walletConnectProvider = new WalletConnectProvider.default({
-    infuraId: INFURA_ID,
-  })
-  await walletConnectProvider.enable()
-  closeWallets()
-  await loadAccount(walletConnectProvider)
-}
-
-async function connectBsc() {
-  if (!window.BinanceChain)
-    return sl('error', 'Please install the Binance Wallet extension')
-  await window.BinanceChain.enable()
-  closeWallets()
-  await loadAccount(window.BinanceChain)
-}
-
-async function setAddress() {
-  if (WRITE_WEB3_PROVIDER) {
-    ADDRESS = await WRITE_WEB3_PROVIDER.getSigner().getAddress()
-    $('#account').html(
-      `${ADDRESS.substring(0, 6)}...${ADDRESS.substring(
-        ADDRESS.length - 4,
-        ADDRESS.length
-      )}`
-    )
-    $('#account_wrapper').removeClass('hidden');
-  }
-}
-
-async function checkIsActive() {
-  IS_ACTIVE = await READ_CONTRACT.isSaleActive()
-}
-
-async function setRemainingTokens() {
-  const remainingTokens = new Big((await READ_CONTRACT.remainingTokens()).toString()).div(
-    new Big(1e18)
-  )
-  const remainingTokensHumanized = toHumanizedCurrency(remainingTokens)
-  $('#remainingTokens').html(remainingTokensHumanized)
-
-  const percentRaised = 100-(remainingTokens.toFixed(0)/TOTAL_FOR_SALE)*100;
-  $('#remainingTokensProgress').attr('style', `width:${percentRaised}%;`)
-}
-
-async function calculateReceiveAmount() {
-  let inputAmount = $('#input-amount').val()
-  if (inputAmount) {
-    const inputAmountInWei = ethers.utils.parseEther(inputAmount)
-    if (inputAmountInWei.isZero()) {
-      return
+async function checkWhitelistStatus() {
+  let inputAddress = $('#input-address').val()
+  if (inputAddress) {
+    const isWhiteListed = _addressList.includes(inputAddress)
+    if (isWhiteListed) {
+      //whitelisted
+      $('#whitelist-status').html("Accepted for whitelist.")
+      $('#whitelist-status').removeClass('cta-btn-blue').addClass('cta-btn-green')
     }
-    try {
-      const receiveAmount = toHumanizedCurrency(
-        new Big(
-          (await READ_CONTRACT._getTokenAmount(inputAmountInWei)).toString()
-        ).div(new Big(1e18))
-      )
-      $('#receive-amount').val(receiveAmount)
-    } catch (error) {
-      console.error(error)
-      sl('error', 'An error occurred. Error: ' + error.reason)
-      $('#receive-amount').val('')
+    else {
+      //sl('success', 'Your wallet is not whitelisted.')
+      $('#whitelist-status').html("Your wallet is not whitelisted.")
+      $('#whitelist-status').removeClass('cta-btn-green').addClass('cta-btn-blue')
     }
   } else {
-    $('#receive-amount').val('')
-  }
-}
-
-async function buyTokens() {
-  if (!IS_ACTIVE) {
-    return sl('error', 'Sale is not active.')
-  }
-  let inputAmount = $('#input-amount').val()
-  if (inputAmount) {
-    let inputAmountInWei = ethers.utils.parseEther(inputAmount)
-    try {
-      setLoading(true, true)
-      await WRITE_CONTRACT.buyTokens(ADDRESS, {
-        value: inputAmountInWei,
-      })
-      setLoading(false)
-      sl(
-        'success',
-        'You will receive your tokens once the transaction has been mined..'
-      )
-      $('input').trigger('reset')
-    } catch (error) {
-      console.error(error)
-      sl('error', 'An error occurred. Please see the console!')
-      setLoading(false)
-    }
-  } else {
-    return
-  }
-}
-
-function toggleBuyButton() {
-  if (WRITE_WEB3_PROVIDER) {
-    $('#connect-wallet-btn').hide()
-    $('#buy-btn').show()
-  } else {
-    $('#connect-wallet-btn').show()
-    $('#buy-btn').hide()
-  }
-  if (!IS_ACTIVE) {
-    $('#buy-btn').text('Sale is not active')
-  }
-}
-
-function setLoading(loading, txnProcessing = false) {
-  LOADING = loading
-
-  if (LOADING) {
-    $('#loader').show()
-    $('#content').hide()
-    if (txnProcessing) {
-      $('#txn-processing-msg').show()
-    }
-  } else {
-    $('#loader').hide()
-    $('#txn-processing-msg').hide()
-    $('#content').show()
+    $('#input-address').val('')
   }
 }
 
@@ -179,13 +34,4 @@ function sl(type, msg) {
     icon: type,
     text: msg,
   })
-}
-
-function toHumanizedCurrency(val) {
-  if (val.toNumber) {
-    return new Big(val.toString()).toFormat(2)
-  }
-  return new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'})
-    .format(val)
-    .replace('$', '')
 }
